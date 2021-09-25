44955e25682a5943bc792ceaab29ec6511a60f86
Aedius <Aedius@users.noreply.github.com>
2021-09-25 20:35:20 +0200

    Don't count string without letter as a word (#1)
    * remove everything without letter from count_words
    
    * add empty line in Cargo.toml
    
    Co-authored-by: Aedius <aedius.filmania@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
 use mdbook::book::{BookItem, Chapter};
 use mdbook::renderer::RenderContext;
 use std::fs::{self, File};
@@ -15,7 +18,14 @@ pub struct WordcountConfig {
 }
 
 fn count_words(ch: &Chapter) -> usize {
-    ch.content.split_whitespace().count()
+    lazy_static! {
+        // a word must have at least one letter in any language
+        static ref RE: Regex = Regex::new(r"\pL").unwrap();
+    }
+    ch.content
+        .split_whitespace()
+        .filter(|&x| RE.is_match(x))
+        .count()
 }
 
 fn get_config(ctx: &RenderContext) -> WordcountConfig {
@@ -50,4 +60,84 @@ fn main() {
     println!("---------");
     println!("Total: {}", total_words);
     writeln!(f, "Total : {}", total_words).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_words_title_punctuation() {
+        let ch = Chapter {
+            name: "".to_string(),
+            content: "# some title ?".to_string(),
+            number: None,
+            sub_items: vec![],
+            path: None,
+            source_path: None,
+            parent_names: vec![],
+        };
+
+        assert_eq!(count_words(&ch), 2);
+    }
+
+    #[test]
+    fn test_count_words_maj() {
+        let ch = Chapter {
+            name: "".to_string(),
+            content: "HELLO WORLD !".to_string(),
+            number: None,
+            sub_items: vec![],
+            path: None,
+            source_path: None,
+            parent_names: vec![],
+        };
+
+        assert_eq!(count_words(&ch), 2);
+    }
+
+    #[test]
+    fn test_count_words_compound() {
+        let ch = Chapter {
+            name: "".to_string(),
+            content: " some-compound-word".to_string(),
+            number: None,
+            sub_items: vec![],
+            path: None,
+            source_path: None,
+            parent_names: vec![],
+        };
+
+        assert_eq!(count_words(&ch), 1);
+    }
+
+    #[test]
+    fn test_count_words_chinese() {
+        let ch = Chapter {
+            name: "".to_string(),
+            content: "中國人".to_string(),
+            number: None,
+            sub_items: vec![],
+            path: None,
+            source_path: None,
+            parent_names: vec![],
+        };
+
+        assert_eq!(count_words(&ch), 1);
+    }
+
+    #[test]
+    fn test_count_words_not_word() {
+        let ch = Chapter {
+            name: "".to_string(),
+            content: "3 🤩 + 🂠 ".to_string(),
+            number: None,
+            sub_items: vec![],
+            path: None,
+            source_path: None,
+            parent_names: vec![],
+        };
+
+        assert_eq!(count_words(&ch), 0);
+    }
 }
